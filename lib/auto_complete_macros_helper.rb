c4a1c27fd2d7c25d45ca5038f205697611784eae
erichhhh <erich2050@gmail.com>
2009-09-01 03:18:55 -0700

    adding the ability to specify the id of the textbox (usefull if you want to tuse auto_complete within activescaffold)

--- lib/auto_complete_macros_helper.rb
+++ lib/auto_complete_macros_helper.rb
@@ -105,10 +105,12 @@ module AutoCompleteMacrosHelper
   # auto_complete_for to respond the AJAX calls,
   # 
   def text_field_with_auto_complete(object, method, tag_options = {}, completion_options = {})
+    tag_options[:id] = "#{object}_#{method}" if !tag_options[:id]
+    
     (completion_options[:skip_style] ? "" : auto_complete_stylesheet) +
     text_field(object, method, tag_options) +
-    content_tag("div", "", :id => "#{object}_#{method}_auto_complete", :class => "auto_complete") +
-    auto_complete_field("#{object}_#{method}", { :url => { :action => "auto_complete_for_#{object}_#{method}" } }.update(completion_options))
+    content_tag("div", "", :id => "#{tag_options[:id]}_auto_complete", :class => "auto_complete") +
+    auto_complete_field("#{tag_options[:id]}", { :url => { :action => "auto_complete_for_#{object}_#{method}" } }.update(completion_options))
   end
 
   private
